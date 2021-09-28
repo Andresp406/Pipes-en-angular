017503e5dbbf2dd21e5bf37321f7f3d5d537aed8
andresp406 <andresp406@hotmail.com>
2021-09-27 21:48:36 -0500

    commit de inicializacion

---
+++ README.md
@@ -0,0 +1,12 @@
+# PipesApp
+
+Esta app corre en el entorno de angular [Angular CLI](https://github.com/angular/angular-cli) version 11.0.2.
+
+## Instalacion de la app
+
+Para la instalacion correcta de este proyecto se necesita tener el angular:CLI instalarlo y luego ejecutar el comando ng serve -o ademas se debe tener instalado el Node.js y dentro de la carpeta donde se encuentra el proyecto se debe ejecutar el comando npm install el cual instalara todas las dependencias de node.
+
+## Datos de la instalacion
+
+En esta aplicacion se muestra el uso de los pipes en angular los cuales nos sirven para mostrar informacion en un formato mas presentable sin modificar la data desde la base de datos.# Pipes-en-angular
+#Pipes-en-angular
---
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-no-comunes',
+  templateUrl: './no-comunes.component.html',
+  styles: [
+  ]
+})
+export class NoComunesComponent  {
+
+  // i18nSelect
+  nombre: string = 'Andres';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino' : 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Hernando', 'Eduardo','Fernando'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente() {
+    this.nombre = 'Melissa';
+    this.genero = 'femenino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+
+  // KeyValue Pipe
+  persona = {
+    nombre: 'Andres',
+    edad: 32,
+    direccion: 'Santa Marta, Colombia'
+  }
+
+  // JsonPipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
+  ]
+
+
+  // Async Pipe
+  miObservable = interval(2000); // 0,1,2,3,4,5,6,
+
+  valorPromesa = new Promise( (resolve, reject) => {
+
+    setTimeout(() => {
+      resolve( 'Tenemos data de promesa' );
+    }, 3500 );
+
+  });
+
+}
